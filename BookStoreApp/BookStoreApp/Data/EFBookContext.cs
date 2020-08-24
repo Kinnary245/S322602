d95bfc5c94aaea1e7d10cc8519bfa6c6eb1a3102
Kinnary245 <kinnary245vasoya@gmail.com>
2020-08-25 07:58:35 +0930

    connection string

---
+++ BookStoreApp/BookStoreApp/Data/EFBookContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using BookStoreApp.DAL;
+
+namespace BookStoreApp.Data
+{
+    public class EFBookContext : DbContext
+    {
+        public EFBookContext(DbContextOptions<EFBookContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Printer> Printers { get; set; }
+    }
+}
